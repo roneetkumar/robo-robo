fcb3e1b996916775c2f414a35e4e9b93ce17f874
Roneet Kumar <roneetparida@gmail.com>
2019-12-06 19:38:46 -0500

    Updates

---
+++ precache-manifest.0661bf04b765522ae98a94196db07a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c4518c75ad8f354f21d331942bf0c2",
+    "url": "/robo-robo/index.html"
+  },
+  {
+    "revision": "3ac4b793de1421df5023",
+    "url": "/robo-robo/static/css/main.e0bc6c07.chunk.css"
+  },
+  {
+    "revision": "eb1e201d3b1e6cf55b67",
+    "url": "/robo-robo/static/js/2.35a93ace.chunk.js"
+  },
+  {
+    "revision": "3ac4b793de1421df5023",
+    "url": "/robo-robo/static/js/main.54ff3208.chunk.js"
+  },
+  {
+    "revision": "03d7b4e77378e3459b24",
+    "url": "/robo-robo/static/js/runtime-main.720f4663.js"
+  }
+]);
--- src/components/Nav/NavListItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NavListItem = (props) => {
-    return (
-        <li>{props.title}</li>
-    )
-}
-
-export default NavListItem;
